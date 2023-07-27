1fcef25772d1f387da8f8f4d4fe05ae655045363
unknown <rrrr0ffff@gmail.com>
2023-07-27 19:59:05 -0300

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Posts from "./components/Posts";
+import { Outlet } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="bg-slate-950 flex flex-col items-center p-10">
 			<div className="container flex flex-col items-center gap-5">
-				<Posts />
+				<Outlet />
 			</div>
 		</div>
 	);
