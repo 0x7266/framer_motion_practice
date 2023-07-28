182756696615d35619b97c7168c78adb38c2b6ec
unknown <rrrr0ffff@gmail.com>
2023-07-28 12:14:29 -0300

    added opacity progress

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -61,15 +61,16 @@ function Post({
 		target: ref,
 		offset: ["0 1", "1 1"],
 	});
-	const scaleProgress = useTransform(scrollYProgress, [0.2, 1], [0.4, 1]);
+	const scaleProgress = useTransform(scrollYProgress, [0.2, 1], [0.95, 1]);
+	const opacityProgress = useTransform(scrollYProgress, [0, 0.2], [0.1, 1]);
 	return (
 		<motion.div
 			style={{
 				scale: scaleProgress,
-				opacity: scrollYProgress,
+				opacity: opacityProgress,
 			}}
 			ref={ref}
-			className="w-3/5 bg-slate-600 flex flex-col gap-5 items-center justify-around border-4 p-10 rounded-xl shadow"
+			className="lg:w-4/5 xl:w-3/5 bg-slate-600 flex flex-col gap-5 items-center justify-around border-4 p-10 rounded-xl shadow"
 		>
 			<div className="text-9xl text-white font-bold uppercase">
 				{post.title}
