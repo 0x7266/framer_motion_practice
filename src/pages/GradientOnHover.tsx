07452b94d68accf17b44154c139eeda119822735
unknown <rrrr0ffff@gmail.com>
2023-07-30 12:44:48 -0300

    refactor

--- src/pages/GradientOnHover.tsx
+++ src/pages/GradientOnHover.tsx
@@ -1,17 +1,16 @@
-import { motion } from "framer-motion";
-import { MouseEvent, useState } from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import { MouseEvent } from "react";
 import HomeIcon from "../assets/HomeIcon";
 import { Link } from "react-router-dom";
 
 export default function Demo() {
-	const [mousePostion, setMousePosition] = useState({ x: 0, y: 0 });
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
 
 	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
 		const { left, top } = currentTarget.getBoundingClientRect();
-
-		const xPosition = clientX - left;
-		const yPosition = clientY - top;
-		setMousePosition({ x: xPosition, y: yPosition });
+		mouseX.set(clientX - left);
+		mouseY.set(clientY - top);
 	}
 
 	return (
@@ -27,9 +26,15 @@ export default function Demo() {
 			</motion.div>
 			<div className="h-full flex flex-col items-center justify-center">
 				<div
-					className="flex flex-col justify-center h-4/5 max-h-[700px]"
+					className="relative flex flex-col justify-center h-4/5 max-h-[700px] group overflow-hidden rounded-3xl"
 					onMouseMove={handleMouseMove}
 				>
+					<motion.div
+						className="pointer-events-none absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-300"
+						style={{
+							background: useMotionTemplate`radial-gradient(450px circle at ${mouseX}px ${mouseY}px, rgba(14, 165, 233, .15), transparent 80%)`,
+						}}
+					/>
 					<motion.div
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
@@ -38,39 +43,33 @@ export default function Demo() {
 							transition: { duration: 1.5, when: "afterChildren" },
 							backgroundColor: "transparent",
 						}}
-						className="relative text-zinc-200 text-7xl font-alfa uppercase bg-gray-900 rounded-3xl p-14 overflow-hidden h-full"
+						className="text-zinc-200 text-7xl font-alfa uppercase bg-gray-900 p-14 overflow-hidden h-full flex flex-col gap-10"
 					>
+						<motion.h1 initial={{ y: -300 }} animate={{ y: 0 }}>
+							HOVER ME
+						</motion.h1>
 						<motion.div
-							className="absolute inset-0"
-							style={{
-								background: `radial-gradient(450px circle at ${mousePostion.x}px ${mousePostion.y}px, rgba(14, 165, 233, .15), transparent 80%)`,
-							}}
-						/>
-						<motion.div className="invisible flex flex-col gap-10">
-							<motion.h1 initial={{ y: -300 }} animate={{ y: 0 }}>
-								HOVER ME
-							</motion.h1>
+							initial="initial"
+							whileInView="inView"
+							transition={{ staggerChildren: 0.2 }}
+							viewport={{ amount: 0.5, once: true }}
+							className="w-full flex flex-col gap-3"
+						>
 							<motion.div
-								initial="initial"
-								whileInView="inView"
-								transition={{ staggerChildren: 0.2 }}
-								viewport={{ amount: 0.5, once: true }}
-								className="w-full flex flex-col gap-3"
-							>
-								<motion.div
-									initial={{ x: "-600px" }}
-									exit={{ x: "-600px" }}
-									animate={{ x: 0 }}
-									transition={{ delay: 0.2, duration: 0.4 }}
-									className="w-full h-6 rounded-lg bg-gray-500"
-								/>
-								<motion.div
-									initial={{ x: "600px" }}
-									exit={{ x: "600px" }}
-									animate={{ x: 0 }}
-									transition={{ delay: 0.3, duration: 0.4 }}
-									className="w-full h-6 rounded-lg bg-gray-500"
-								/>
+								initial={{ x: "-600px" }}
+								exit={{ x: "-600px" }}
+								animate={{ x: 0 }}
+								transition={{ delay: 0.2, duration: 0.4 }}
+								className="w-full h-6 rounded-lg bg-gray-500"
+							/>
+							<motion.div
+								initial={{ x: "600px" }}
+								exit={{ x: "600px" }}
+								animate={{ x: 0 }}
+								transition={{ delay: 0.3, duration: 0.4 }}
+								className="w-full h-6 rounded-lg bg-gray-500"
+							/>
+							<div className="flex gap-3">
 								<motion.div
 									initial={{ x: "-600px" }}
 									exit={{ x: "-600px" }}
@@ -78,7 +77,38 @@ export default function Demo() {
 									transition={{ delay: 0.5, duration: 0.4 }}
 									className="w-full h-6 rounded-lg bg-gray-500"
 								/>
-							</motion.div>
+								<motion.div
+									initial={{ x: "600px" }}
+									exit={{ x: "600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.5, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+							</div>
+							<motion.div
+								initial={{ x: "-600px" }}
+								exit={{ x: "-600px" }}
+								animate={{ x: 0 }}
+								transition={{ delay: 0.7, duration: 0.4 }}
+								className="w-full h-6 rounded-lg bg-gray-500"
+							/>
+							<div className="flex gap-3">
+								<motion.div
+									initial={{ x: "-600px" }}
+									exit={{ x: "-600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.9, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+								<motion.div
+									initial={{ x: "600px" }}
+									exit={{ x: "600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.9, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+							</div>
+							<div className="h-60 w-full bg-violet-900 mix-blend-darken rounded-lg" />
 						</motion.div>
 					</motion.div>
 				</div>
