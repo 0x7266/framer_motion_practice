dce1f39ab2ba0b598b61b07175f0e63caed73fac
unknown <rrrr0ffff@gmail.com>
2023-07-30 12:01:25 -0300

    getting x and y position

--- src/pages/GradientOnHover.tsx
+++ src/pages/GradientOnHover.tsx
@@ -1,8 +1,16 @@
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { MouseEvent } from "react";
 import HomeIcon from "../assets/HomeIcon";
+import { Link } from "react-router-dom";
 
-export default function GradientOnHover() {
+export default function Demo() {
+	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+		const { left, top } = currentTarget.getBoundingClientRect();
+
+		const xPosition = clientX - left;
+		const yPosition = clientY - top;
+	}
+
 	return (
 		<>
 			<motion.div
@@ -14,50 +22,64 @@ export default function GradientOnHover() {
 					<HomeIcon />
 				</Link>
 			</motion.div>
-			<div className="w-full h-full flex flex-col items-center justify-center">
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{
-						opacity: 0,
-						transition: { duration: 1.5, when: "afterChildren" },
-					}}
-					// transition={{ staggerChildren: 2 }}
-					className="flex flex-col gap-10 text-zinc-200 text-7xl font-alfa uppercase bg-zinc-700 rounded-lg p-5 h-3/5 max-h-[500px] overflow-hidden"
+			<div className="h-full flex flex-col items-center justify-center">
+				<div
+					className="flex flex-col justify-center h-4/5 max-h-[700px]"
+					onMouseMove={handleMouseMove}
 				>
-					<motion.h1 initial={{ y: -300 }} animate={{ y: 0 }}>
-						HOVER ME
-					</motion.h1>
 					<motion.div
-						initial="initial"
-						whileInView="inView"
-						transition={{ staggerChildren: 0.2 }}
-						viewport={{ amount: 0.5, once: true }}
-						className="w-full flex flex-col gap-3"
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{
+							opacity: 0,
+							transition: { duration: 1.5, when: "afterChildren" },
+							backgroundColor: "transparent",
+						}}
+						className="relative text-zinc-200 text-7xl font-alfa uppercase bg-gray-900 rounded-3xl p-14 overflow-hidden h-full"
 					>
 						<motion.div
-							initial={{ x: "-600px" }}
-							exit={{ x: "-600px" }}
-							animate={{ x: 0 }}
-							transition={{ delay: 0.2, duration: 0.8 }}
-							className="w-full h-6 rounded-lg bg-gray-500"
+							className="absolute inset-0"
+							style={{
+								background:
+									"radial-gradient(circle at 0px 0px, rgba(14, 165, 233, 1), transparent 80%)",
+							}}
 						/>
-						<motion.div
-							initial={{ x: "600px" }}
-							exit={{ x: "600px" }}
-							animate={{ x: 0 }}
-							transition={{ delay: 0.3, duration: 0.8 }}
-							className="w-full h-6 rounded-lg bg-gray-500"
-						/>
-						<motion.div
-							initial={{ x: "-600px" }}
-							exit={{ x: "-600px" }}
-							animate={{ x: 0 }}
-							transition={{ delay: 0.5, duration: 0.8 }}
-							className="w-full h-6 rounded-lg bg-gray-500"
-						/>
+						<motion.div className="invisible flex flex-col gap-10">
+							<motion.h1 initial={{ y: -300 }} animate={{ y: 0 }}>
+								HOVER ME
+							</motion.h1>
+							<motion.div
+								initial="initial"
+								whileInView="inView"
+								transition={{ staggerChildren: 0.2 }}
+								viewport={{ amount: 0.5, once: true }}
+								className="w-full flex flex-col gap-3"
+							>
+								<motion.div
+									initial={{ x: "-600px" }}
+									exit={{ x: "-600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.2, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+								<motion.div
+									initial={{ x: "600px" }}
+									exit={{ x: "600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.3, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+								<motion.div
+									initial={{ x: "-600px" }}
+									exit={{ x: "-600px" }}
+									animate={{ x: 0 }}
+									transition={{ delay: 0.5, duration: 0.4 }}
+									className="w-full h-6 rounded-lg bg-gray-500"
+								/>
+							</motion.div>
+						</motion.div>
 					</motion.div>
-				</motion.div>
+				</div>
 			</div>
 		</>
 	);
