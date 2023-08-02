288910ff08dc895a41ddeb82fda831094d89fed9
unknown <rrrr0ffff@gmail.com>
2023-08-02 19:23:36 -0300

    added mask

--- src/pages/MaskOnHover.tsx
+++ src/pages/MaskOnHover.tsx
@@ -1,10 +1,22 @@
+import mask from "../assets/mask.svg";
+
 export function MaskOnHover() {
 	return (
-		<div className="w-full flex flex-col items-center text-8xl font-bold main">
-			<div className="absolute h-screen grid place-content-center mask w-full">
+		<div className="w-full flex flex-col items-center text-8xl font-bold">
+			<div
+				className="absolute h-screen bg-violet-500 grid place-content-center w-full"
+				style={{
+					maskImage: `url(${mask})`,
+					WebkitMaskImage: `url(${mask})`,
+					maskRepeat: "no-repeat",
+					WebkitMaskRepeat: "no-repeat",
+					maskSize: "50px",
+					WebkitMaskSize: "50px",
+				}}
+			>
 				<div>MASKMASKMASKMASKMASKMASK</div>
 			</div>
-			<div className="h-screen grid place-content-center body">MaskOnHover</div>
+			<div className="h-screen grid place-content-center">MaskOnHover</div>
 		</div>
 	);
 }
