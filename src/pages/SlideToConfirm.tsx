1aca3b63cd808e8462a55d23e0b613f6924549a0
unknown <rrrr0ffff@gmail.com>
2023-08-04 12:33:29 -0300

    centered component

--- src/pages/SlideToConfirm.tsx
+++ src/pages/SlideToConfirm.tsx
@@ -3,14 +3,16 @@ import { motion } from "framer-motion";
 export default function SlideToConfirm() {
 	return (
 		<>
-			<div className="bg-sky-700 w-96 rounded-full flex justify-center items-center text-2xl text-zinc-200 relative h-20">
-				<motion.div
-					drag="x"
-					className="flex items-center justify-center bg-cyan-800 p-5 rounded-full h-16 w-16 text-6xl absolute left-2"
-				>
-					<span className="relative -top-2 left-[2px]">&#187;</span>
-				</motion.div>
-				<span className="m-5 font-bold">SLIDE TO CONFIRM</span>
+			<div className="h-screen flex flex-col items-center justify-center">
+				<div className="bg-sky-700 w-[500px] rounded-full flex justify-center items-center text-2xl text-zinc-200 relative h-20">
+					<motion.div
+						drag="x"
+						className="bg-cyan-800 p-5 rounded-full h-16 w-16 text-6xl"
+					>
+						<span className="relative -top-[27px]">&#187;</span>
+					</motion.div>
+					<span className="m-5 font-bold">SLIDE TO CONFIRM</span>
+				</div>
 			</div>
 		</>
 	);
