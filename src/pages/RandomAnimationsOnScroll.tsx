f349648867f8bc256e9927ca4454a77b92dd563d
unknown <rrrr0ffff@gmail.com>
2023-08-08 12:03:59 -0300

    added bordder radius

--- src/pages/RandomAnimationsOnScroll.tsx
+++ src/pages/RandomAnimationsOnScroll.tsx
@@ -4,7 +4,7 @@ export default function RandomAnimationOnScroll() {
 	const { scrollYProgress } = useScroll();
 	const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
 	const scale = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
-	const borderRadius = useTransform(scrollYProgress, [0, 1], [0, "100%"]);
+	const borderRadius = useTransform(scrollYProgress, [1, 0], [0, 120]);
 	return (
 		<div className="min-h-[300dvh] bg-slate-900 flex flex-col items-center">
 			<motion.div
@@ -13,7 +13,7 @@ export default function RandomAnimationOnScroll() {
 			/>
 			<motion.div
 				className="fixed top-20 w-60 h-60 bg-rose-300"
-				style={{ rotate, scale }}
+				style={{ rotate, scale, borderRadius }}
 			/>
 		</div>
 	);
