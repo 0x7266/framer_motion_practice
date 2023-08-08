cbdc4a055d7990993d8d900fb76d0a6c8df0c85b
unknown <rrrr0ffff@gmail.com>
2023-08-07 23:54:56 -0300

    added basic rotate animation on scroll

--- src/pages/RandomAnimationsOnScroll.tsx
+++ src/pages/RandomAnimationsOnScroll.tsx
@@ -1,7 +1,11 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+
 export default function RandomAnimationOnScroll() {
+	const { scrollYProgress } = useScroll();
+	const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
 	return (
-		<div className="min-h-[300dvh] bg-slate-900">
-			random animations on scroll
+		<div className="min-h-[300dvh] bg-slate-900 flex flex-col items-center justify-center">
+			<motion.div className="w-60 h-60 bg-rose-300" style={{ rotate }} />
 		</div>
 	);
 }
