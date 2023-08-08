be4fb3f1a00b24d5322942f63a75333ffe7c452a
unknown <rrrr0ffff@gmail.com>
2023-08-08 11:58:38 -0300

    added progress bar on top

--- src/pages/RandomAnimationsOnScroll.tsx
+++ src/pages/RandomAnimationsOnScroll.tsx
@@ -5,6 +5,10 @@ export default function RandomAnimationOnScroll() {
 	const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
 	return (
 		<div className="min-h-[300dvh] bg-slate-900 flex flex-col items-center justify-center">
+			<motion.div
+				className="fixed top-0 left-0 right-0 h-3 bg-rose-500 self-start"
+				style={{ scaleX: scrollYProgress, transformOrigin: "0%" }}
+			/>
 			<motion.div className="w-60 h-60 bg-rose-300" style={{ rotate }} />
 		</div>
 	);
