889a30f29b0ae84c37506004723b7877f04c5231
unknown <rrrr0ffff@gmail.com>
2023-08-08 12:01:54 -0300

    added scale

--- src/pages/RandomAnimationsOnScroll.tsx
+++ src/pages/RandomAnimationsOnScroll.tsx
@@ -3,13 +3,18 @@ import { motion, useScroll, useTransform } from "framer-motion";
 export default function RandomAnimationOnScroll() {
 	const { scrollYProgress } = useScroll();
 	const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
+	const scale = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
+	const borderRadius = useTransform(scrollYProgress, [0, 1], [0, "100%"]);
 	return (
-		<div className="min-h-[300dvh] bg-slate-900 flex flex-col items-center justify-center">
+		<div className="min-h-[300dvh] bg-slate-900 flex flex-col items-center">
 			<motion.div
-				className="fixed top-0 left-0 right-0 h-3 bg-rose-500 self-start"
+				className="fixed top-0 left-0 right-0 h-3 bg-rose-500 self-start z-10"
 				style={{ scaleX: scrollYProgress, transformOrigin: "0%" }}
 			/>
-			<motion.div className="w-60 h-60 bg-rose-300" style={{ rotate }} />
+			<motion.div
+				className="fixed top-20 w-60 h-60 bg-rose-300"
+				style={{ rotate, scale }}
+			/>
 		</div>
 	);
 }
